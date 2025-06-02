98030c4cdbb27a9df69bca59ca0208bba5a88a1f
blackbird77ad <dhavyb.h@gmail.com>
2025-06-02 13:25:54 +0000

    Deployment fix

--- index.mjs
+++ index.mjs
@@ -13,7 +13,7 @@ try {
     await mongoose.connect(MONGODB_URI);
     console.log('Connected to MongoDB');
 } catch (error) {
-    console.error('Failed to connect to MongoDB:', err);
+    console.error('Failed to connect to MongoDB:', error);
 }
 
 
